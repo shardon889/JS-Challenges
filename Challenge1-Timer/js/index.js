1f80685e8b3f213cd25bc10ea019591715c6202a
Paras Setia <parassetia889@gmail.com>
2022-06-24 12:48:04 +0530

    fixing the errors

--- Challenge1-Timer/js/index.js
+++ Challenge1-Timer/js/index.js
@@ -1,8 +1,8 @@
 import {
-  changeColorToRed,
-  toggleTimer,
-  toggleTimerText,
-  editTime,
+    changeColorToRed,
+    toggleTimer,
+    toggleTimerText,
+    editTime,
 } from './toggler.js';
 import {getMinute, getSecond} from './get.js';
 
@@ -18,44 +18,44 @@ gear.addEventListener('click', editTime);
 
 // perform actions when time is up
 const timeUp = () => {
-  clearInterval(timerId);
-  changeColorToRed();
-  toggleTimerText();
-  setTimeout(function() {
-    alert('Time\'s up.');
-  }, 1);
+    clearInterval(timerId);
+    changeColorToRed();
+    toggleTimerText();
+    setTimeout(function() {
+        alert('Time\'s up.');
+    }, 1);
 };
 
 const reduceTimeEachSecond = () => {
-  const minuteField = getMinute();
-  const secondTextField = getSecond();
-  let seconds = secondTextField.value;
-  let minutes = minuteField.value;
+    const minuteField = getMinute();
+    const secondTextField = getSecond();
+    let seconds = secondTextField.value;
+    let minutes = minuteField.value;
 
-  if (seconds == 0) {
-    if (minutes > 0) {
-      minutes--;
-      seconds = '59';
-      // eslint-disable-next-line brace-style
-    } // minutes == 0 seconds == 0
-    else {
-      timeUp();
+    if (seconds == 0) {
+        if (minutes > 0) {
+            minutes--;
+            seconds = '59';
+            // eslint-disable-next-line brace-style
+        } // minutes == 0 seconds == 0
+        else {
+            timeUp();
+        }
+    } else {
+        seconds--;
     }
-  } else {
-    seconds--;
-  }
 
-  minuteField.value = check(minutes);
-  secondTextField.value = check(seconds);
+    minuteField.value = check(minutes);
+    secondTextField.value = check(seconds);
 };
 
 // const to perform time reduction every second
 const reduceTimer = () => {
-  timerId = setInterval(reduceTimeEachSecond, 1000);
+    timerId = setInterval(reduceTimeEachSecond, 1000);
 };
 
 const check = (value) => {
-  if (value >= 0 && value <= 9) value = '0' + (value % 10);
-  return value;
+    if (value >= 0 && value <= 9) value = '0' + (value % 10);
+    return value;
 };
 export {startButton, gear, timerId, timeUp, reduceTimer};
