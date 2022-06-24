fc2ec30a453b5b0fd443c561abef00ec77e1757d
Paras Setia <parassetia889@gmail.com>
2022-06-24 12:55:50 +0530

    fixing the errors

--- Challenge2-ShoppingCart/src/js/index.js
+++ Challenge2-ShoppingCart/src/js/index.js
@@ -3,9 +3,9 @@ import {addToCart} from './utilities.js';
 const addButtons = document.querySelectorAll('.add');
 
 const addListener = () => {
-  addButtons.forEach((addButton) => {
-    addButton.addEventListener('click', addToCart);
-  });
+    addButtons.forEach((addButton) => {
+        addButton.addEventListener('click', addToCart);
+    });
 };
 
 addListener();
